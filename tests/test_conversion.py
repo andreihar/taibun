8143c21962d1302df4d7086f8f9ec066a071283b
andreihar <95883512+andreihar@users.noreply.github.com>
2023-06-17 01:25:32 -0700

    Fixed test cases reflecting new method names

--- tests/test_conversion.py
+++ tests/test_conversion.py
@@ -7,21 +7,21 @@ class TestConversion(unittest.TestCase):
         hanji = "先生講、學生恬恬聽。"
         system = "POJ"
         expected = "Sian-siⁿ kóng, ha̍k-seng tiām-tiām thiaⁿ."
-        result = taibun.converter(hanji, system=system)
+        result = taibun.get(hanji, system=system)
         self.assertEquals(result, expected)
 
     def test_hanji_to_tailo(self):
         hanji = "先生講、學生恬恬聽。"
         system = "Tai-lo"
         expected = "Sian-sinn kóng, ha̍k-sing tiām-tiām thiann."
-        result = taibun.converter(hanji, system=system)
+        result = taibun.get(hanji, system=system)
         self.assertEquals(result, expected)
 
     def test_hanji_to_zhuyin(self):
         hanji = "先生講、學生恬恬聽。"
         system = "zhuyin"
         expected = "ㄒㄧㄢ ㄒㆪ ㄍㆲˋ, ㄏㄚㆶ˙ ㄒㄧㄥ ㄉㄧㆰ˫ ㄉㄧㆰ˫ ㄊㄧㆩ."
-        result = taibun.converter(hanji, system=system)
+        result = taibun.get(hanji, system=system)
         self.assertEquals(result, expected)
 
     def test_tailo_to_zhuyin(self):
