ea6004ac85134754c12c6d3b390f2f7ab666abff
andreihar <95883512+andreihar@users.noreply.github.com>
2023-09-04 14:38:37 -0700

    Fixing Pingyim test cases

--- tests/test_pingyim_conversion.py
+++ tests/test_pingyim_conversion.py
@@ -7,7 +7,7 @@ c_north = Converter(system="Pingyim", dialect="north", punctuation='none')
 def test_pingyim_initials():
     bilabial = ["邊,biān","波,pō","毛,bbnoó","文,bbún"]
     checker(bilabial, c, c_north)
-    alveolar = ["地,dê/duê","他,tnā","曾,zān","出,cút","衫,snā","耐,lnâi","熱衫,zzuáh-snā/luáh-snā","柳,liǔ"]
+    alveolar = ["地,dê/duê","他,tnā","曾,zān","出,cūt","衫,snā","耐,nâi","熱衫,zzuáhsnā/luáhsnā","柳,liǔ"]
     checker(alveolar, c, c_north)
     alveolo_palatal = ["尖,ziām","手,ciǔ","寫,siǎ","入,zzíp/líp"]
     checker(alveolo_palatal, c, c_north)
@@ -15,9 +15,9 @@ def test_pingyim_initials():
     checker(velar, c, c_north)
 
 def test_pingyim_vowels_and_rhymes():
-    front = ["衣,yī","禮,lě","圓,ní","生死,snē-sí/snī-sí"]
+    front = ["衣,yī","禮,lě","圓,ní","生死,snēsí/snīsí"]
     checker(front, c, c_north)
-    central = ["高,gō","查問,zá-bûn","衫,snā"]
+    central = ["高,gō","查問,zábûn","衫,snā"]
     checker(central, c, c_north)
     back = ["污,ū","烏,oō","張,dniū","唔,noō"]
     checker(back, c, c_north)
