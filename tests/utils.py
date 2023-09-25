65b1bb0bdd1b50cdd7416319c244eac61f784325
andreihar <95883512+andreihar@users.noreply.github.com>
2023-09-24 20:32:18 -0700

    Added Format test cases

--- tests/utils.py
+++ tests/utils.py
@@ -1,11 +1,11 @@
 def checker(array, general_converter, north_converter):
     for word in array:
         hanji = word.split(',')[0]
-        zhuyin = word.split(',')[1].strip()
-        if '/' in zhuyin: zhuyin = zhuyin.split('/')
-        else: zhuyin = [zhuyin]
-        if len(zhuyin) == 2:
-            assert zhuyin[0] == general_converter.get(hanji)
-            assert zhuyin[1] == north_converter.get(hanji)
+        transl = word.split(',')[1].strip()
+        if '/' in transl: transl = transl.split('/')
+        else: transl = [transl]
+        if len(transl) == 2:
+            assert transl[0] == general_converter.get(hanji)
+            assert transl[1] == north_converter.get(hanji)
         else:
-            assert zhuyin[0] == general_converter.get(hanji)
+            assert transl[0] == general_converter.get(hanji)
