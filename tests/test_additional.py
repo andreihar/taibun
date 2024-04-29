5f86066b2880fb6db61cd9e22e1acd7d376be692
andreihar <95883512+andreihar@users.noreply.github.com>
2024-04-29 14:00:02 -0700

    Added to_simplified to additional tests

--- tests/test_additional.py
+++ tests/test_additional.py
@@ -1,8 +1,14 @@
-from taibun.taibun import Converter, Tokeniser, is_cjk, to_traditional
+from taibun.taibun import Converter, Tokeniser, is_cjk, to_traditional, to_simplified
 
 def test_convert_simplified():
 	c = Converter()
 	assert(c.get('我爱学语言')) == 'Guá ài o̍h gí-giân'
+
+def test_to_traditional():
+	assert '漢字是台灣用來寫幾若種現代佮古代語文个書寫文字系統' == to_traditional('汉字是台湾用来写几若种现代佮古代语文个书写文字系统')
+
+def test_to_simplified():
+	assert '汉字是台湾用来写几若种现代佮古代语文个书写文字系统' == to_simplified('漢字是臺灣用來寫幾若種現代佮古代語文个書寫文字系統')
 
 def test_is_cjk():
 	assert is_cjk('漢') == True
@@ -12,17 +18,3 @@ def test_is_cjk():
 	assert is_cjk('福建') == True
 	assert is_cjk('𥴊仔賴') == True
 	assert is_cjk('福建a') == False
-
-def test_to_traditional():
-	assert '漢字是用來寫幾若種現代佮古代語文个書寫文字系統' == to_traditional('汉字是用来写幾若种现代佮古代语文个书写文字系统')
-
-
-def test_suffix():
-	t = Tokeniser()
-	assert ['咱', '的', '食飯', '是', '誠好', '食'] == t.tokenise("咱的食飯是誠好食")
-	assert ['卯死', '矣'] == t.tokenise("卯死矣")
-
-def test_simplified():
-	t = Tokeniser()
-	assert ['汉字', '是', '用来', '写', '几', '若', '种', '现代', '佮', '古代', '语文', '个', '书写', '文字', '系统', '。'] == t.tokenise('汉字是用来写几若种现代佮古代语文个书写文字系统。')
-	assert ['现代', '个', '中国', '、', '日本', '、', '韩国', '、', '台湾', '拢', '有', '使用', '汉字'] == t.tokenise('现代个中国、日本、韩国、台湾拢有使用汉字')
