453048a991dd7c5b0b2ee2fce3276e88ae9e33e6
andreihar <95883512+andreihar@users.noreply.github.com>
2024-05-26 15:20:08 -0700

    Fixed unit tests for new tokenisation method

--- tests/test_tokenisation.py
+++ tests/test_tokenisation.py
@@ -5,9 +5,15 @@ def test_general():
 	assert ['太空', '朋友', '，', '恁好', '！', '恁', '食飽', '未', '？'] == t.tokenise("太空朋友，恁好！恁食飽未？")
 	assert ['漢字', '（', '閩南語', '注音', ':', 'ㄏㄢˋ', 'ㆡㄧˉ', '；', '白話字', ':', 'Hàn-jī', '；'] == t.tokenise('漢字（閩南語注音: ㄏㄢˋ ㆡㄧˉ；白話字: Hàn-jī；')
 
+def test_best_solution_tokenisation():
+    t = Tokeniser()
+    assert ['中國', '人民', '共和國'] == t.tokenise("中國人民共和國")
+    assert ['中國人', '民雄', '協會'] == t.tokenise('中國人民雄協會')
+    assert ['花蓮', '市議員'] == t.tokenise('花蓮市議員')
+
 def test_suffix():
 	t = Tokeniser()
-	assert ['咱', '的', '食飯', '是', '誠好', '食'] == t.tokenise("咱的食飯是誠好食")
+	assert ['咱', '的', '食飯', '是', '誠', '好食'] == t.tokenise("咱的食飯是誠好食")
 	assert ['卯死', '矣'] == t.tokenise("卯死矣")
 
 def test_simplified():
