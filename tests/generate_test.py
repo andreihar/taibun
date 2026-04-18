381dbbfd8631b8b9c7ae8ddce6912f45f5eccacc
andreihar <95883512+andreihar@users.noreply.github.com>
2026-04-18 16:32:14 +0100

    Updated generate_test

--- tests/generate_test.py
+++ tests/generate_test.py
@@ -1,19 +1,21 @@
-from taibun.taibun import Converter
 import json
 
-system = ['Tailo', 'POJ', 'Zhuyin', 'TLPA', 'Pingyim', 'Tongiong']
+from taibun.taibun import Converter
+
+system = ['Tailo', 'POJ', 'Zhuyin', 'TLPA', 'Pingyim', 'Tongiong', 'IPA']
 hanji_data = ["廟尪仔","翁某仔","肉幼仔","花搭仔","微微仔","慢慢仔","尾蝶仔"]
 
 format = 'mark'
 delimiter = '-'
+apostrophe = True
 sandhi = 'auto'
 punctuation = 'format'
 convert_non_cjk = True
 
 with open('output.txt', 'w', encoding='utf-8') as f:
     for i, s in enumerate(system):
-        c_south = Converter(system=s, format=format, delimiter=delimiter, sandhi=sandhi, punctuation=punctuation, convert_non_cjk = convert_non_cjk)
-        c_north = Converter(system=s, dialect="north", format=format, delimiter=delimiter, sandhi=sandhi, punctuation=punctuation, convert_non_cjk = convert_non_cjk)
+        c_south = Converter(system=s, format=format, delimiter=delimiter, apostrophe=apostrophe, sandhi=sandhi, punctuation=punctuation, convert_non_cjk = convert_non_cjk)
+        c_north = Converter(system=s, dialect="north", format=format, delimiter=delimiter, apostrophe=apostrophe, sandhi=sandhi, punctuation=punctuation, convert_non_cjk = convert_non_cjk)
         transl = []
         for hanji in hanji_data:
             south = c_south.get(hanji)
